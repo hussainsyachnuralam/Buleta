07da232f51e0731b6614681d832fbf19fa0d1801
hussainsyachnuralam <husinnuralam07@gmail.com>
2022-12-04 23:25:12 +0700

    update

--- buleta_flutter/lib/screens/splashscreen.dart
+++ buleta_flutter/lib/screens/splashscreen.dart
@@ -1,1 +1,46 @@
+import 'package:buleta_flutter/screens/home.dart';
+import 'package:buleta_flutter/widgets/navbar.dart';
+import 'package:flutter/material.dart';
 
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // set time to load the new page
+    Future.delayed(Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => NavBAr()));
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 300,
+              width: 300,
+              child: Image.asset("assets/palangkaraya.png"),
+            ),
+            Text(
+              "Eventder",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
