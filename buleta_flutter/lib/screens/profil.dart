cd90d7c45017442adcdb39cca91cf47a390d1a57
hussainsyachnuralam <husinnuralam07@gmail.com>
2022-12-05 01:37:01 +0700

    update profil media center

--- buleta_flutter/lib/screens/profil.dart
+++ buleta_flutter/lib/screens/profil.dart
@@ -28,12 +28,33 @@ class _ProfilState extends State<Profil> {
                 ),
               ),
             ),
-            Image.asset("assets/baliho.jpg"),
+            Image.asset("assets/baliho_mc.jpg"),
             Container(
               padding: EdgeInsets.symmetric(horizontal: 10.0),
-              child: Text(
-                "One, two, three 지금부턴 다 surprise (Uh-huh) A to Z 너를 위한 이 순간 Just today 뭐든 해도 되니까 (Yeah) R to V 우릴 따라와 봐 (Come on) 들뜨는 기분에 완벽한 날씨까지 (Lets get lit) Feel so high, oh my gosh 전부 선물 같지Dumb, dumb (날뛰는 기분이야) 오늘 하룬 마치 diamonds (Diamonds)",
-                style: TextStyle(fontSize: 20.0),
+              child: Column(
+                children: [
+                  Text(
+                    "Berdasarkan Peraturan Daerah Kota Palangka Raya Nomor : 11 Tahun 2016 tentang Pembentukan dan Susunan Perangkat Daerah Kota Palangka Raya dan Peraturan Menteri Komunikasi dan Informatika RI Nomor : 14 Tahun 2016 tentang Pedoman Nomenklatur Perangkat Daerah Bidang Komunikasi dan Informatika, maka Dinas Komunikasi, Informatika, Statistik dan Persandian Kota Palangka Raya memiliki peran yang sangat strategis dalam pengembangan Teknologi Informasi dan Komunikasi (TIK), mengingat perkembangan TIK saat ini semakin pesat, dapat dipastikan pemanfaatan TIK oleh masyarakat sudah menjadi kebutuhan dalam menjalankan aktivitasnya.",
+                    style: TextStyle(fontSize: 16.0),
+                    textAlign: TextAlign.justify,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "Sebagai tindak lanjut dari Surat Menteri Komunikasi dan Informatika RI Nomor : s-1923/M.Kominfo /PL.04.01/01/2016 Tanggal 16 Desember 2016 intinya Pemerintah Kota Palangka Raya dalam hal ini Dinas Komunikasi, Informatika, Statistik dan Persandian Kota Palangka Raya mendapat program bantuan hibah peralatan media center, selanjutnya pada Tanggal 31 Januari 2017 Direktorat Jenderal Informasi dan Komunikasi Publik Kementerian Komunikasi dan Informatika mengundang Kepala Dinas Komunikasi, Informatika, Statistik dan Persandian Kota Palangka Raya bersama dengan 25 Pejabat Pemerintah Kabupaten/Kota se Indonesia untuk melakukan penanda tanganan Berita Acara Serah Terima dan Naskah Perjanjian Hibah Media Center TA. 2016 bertempat diruang rapat melalui Gedung B Lantai 2 Kementerian Komunikasi dan Informatika jl. Medan Merdeka Barat No.09 Jakarta Pusat.",
+                    style: TextStyle(fontSize: 16.0),
+                    textAlign: TextAlign.justify,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "Tujuan pembangunan dan pengembangan media center adalah untuk mendukung pelaksanaan tugas lembaga pemerintah daerah khususnya dalam penyebaran informasi untuk kebutuhan publik dan juga sebagai upaya mengembangkan jaringan pertukaran informasi antara lembaga Kabupaten/Kota, Pemerintah Provinsi dan Kementerian/Lembaga. Media center dirancang untuk mengembangkan pelayanan informasi kepada publik sebagai bagian dari upaya mendorong masyarakat dalam mendapatkan informasi yang mudah, cepat dan terjangkau. Media Center Isen Mulang Kota Palangka Raya mulai operasional pada tanggal 1 Maret 2017.",
+                    style: TextStyle(fontSize: 16.0),
+                    textAlign: TextAlign.justify,
+                  ),
+                ],
               ),
             ),
           ],
