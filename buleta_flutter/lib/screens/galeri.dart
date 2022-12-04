8f0e168294497bb6f7f7d7fc8062b7d814de7738
hussainsyachnuralam <husinnuralam07@gmail.com>
2022-12-05 05:20:45 +0700

    update

--- buleta_flutter/lib/screens/galeri.dart
+++ buleta_flutter/lib/screens/galeri.dart
@@ -10,11 +10,30 @@ class Galeri extends StatefulWidget {
 class _GaleriState extends State<Galeri> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Text("Galeri"),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey,
+          title: Text("Galeri"),
+        ),
       ),
+      debugShowCheckedModeBanner: false,
     );
+  }
+}
+
+class GambarPage extends StatefulWidget {
+  GambarPage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<GambarPage> createState() => _GambarPageState();
+}
+
+class _GambarPageState extends State<GambarPage> {
+  List<String> values = [];
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
