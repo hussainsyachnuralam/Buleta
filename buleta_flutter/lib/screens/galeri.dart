aaa9b49d69822ae4d84f3c9cefd8f7ff025ef6ea
hussainsyachnuralam <husinnuralam07@gmail.com>
2022-12-05 05:40:24 +0700

    update

--- buleta_flutter/lib/screens/galeri.dart
+++ buleta_flutter/lib/screens/galeri.dart
@@ -1,3 +1,4 @@
+import 'package:buleta_flutter/screens/gambar.dart';
 import 'package:flutter/material.dart';
 
 class Galeri extends StatefulWidget {
@@ -31,9 +32,50 @@ class GambarPage extends StatefulWidget {
 }
 
 class _GambarPageState extends State<GambarPage> {
-  List<String> values = [];
+  List<String> values = [
+    'assets/satu.jpg',
+    'assets/dua.jpg',
+    'assets/tiga.jpeg',
+    'assets/empat.jpeg',
+    'assets/lima.jpeg',
+    'assets/enam.jpeg',
+    'assets/tujuh.jpg',
+    'assets/delapan.jpeg',
+    'assets/sembilan.jpeg',
+    'assets/sepuluh.jpeg',
+    'assets/sebelas.jpeg',
+    'assets/duabelas.jpeg',
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Section 18'),
+        ),
+        body: Container(
+          child: GridView.builder(
+            itemCount: 12,
+            itemBuilder: (context, index) {
+              return InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: ((context) => Gambar(
+                                img: values[index],
+                              ))));
+                },
+                child: Card(
+                  elevation: 10,
+                  child: Center(
+                    child: Image.asset(values[index]),
+                  ),
+                ),
+              );
+            },
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          ),
+        ));
   }
 }
